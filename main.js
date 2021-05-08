e9f60e00885c0568bc0fc79265ff0ae9ee0b29f8
GeorgeShao <GeorgeShao123@gmail.com>
2021-05-08 14:49:27 -0400

    Initial lib & dcp setup commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var test = require('./test.js');
+
+var matrix_a = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+
+var matrix_b = [
+    [10, 11],
+    [20, 21],
+    [30, 31]
+];
+
+var c = 2
+var d = 3
+
+console.log(test.multiply(2, 3))
